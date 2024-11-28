b580e5ac53c5c5fe4d57b78a60ba53305dfea888
LeoCodeRu <leo@leocode.ru>
2024-11-28 20:48:06 +0300

    Add proj with test async / await

--- spirit/packages/data_layer/lib/src/repository/info_repo.dart
+++ spirit/packages/data_layer/lib/src/repository/info_repo.dart
@@ -1,13 +1,19 @@
 import 'dart:isolate';
 
+Future<void> getInfo(SendPort sendPort) async {
+  double total = 0.0;
+  for (double i = 0; i < 1000; i++) total += i;
+  sendPort.send(total);
+}
+
 class InfoRepo {
-  Future<void> getInfo1(SendPort sendPort) async {
+  static Future<void> getInfo1(SendPort sendPort) async {
     double total = 0.0;
     for (double i = 0; i < 1000; i++) total += i;
     sendPort.send(total);
   }
 
-  Future<void> getInfo2(SendPort sendPort) async {
+  static Future<void> getInfo2(SendPort sendPort) async {
     double total = 0.0;
     for (double i = 0; i < 50000000; i++) total += i;
     sendPort.send(total);
